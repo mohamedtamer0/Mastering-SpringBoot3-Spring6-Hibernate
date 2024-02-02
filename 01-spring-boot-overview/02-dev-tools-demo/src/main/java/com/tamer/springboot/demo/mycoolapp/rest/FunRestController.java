dc6a2b349383dd070d41f9d77cbcfb279deaa790
mohamedtamer0 <mt4452242@gmail.com>
2024-02-02 14:25:55 +0200

    SpringBoot

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/tamer/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,12 @@
+package com.tamer.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World From Spring Boot";
+    }
+}
