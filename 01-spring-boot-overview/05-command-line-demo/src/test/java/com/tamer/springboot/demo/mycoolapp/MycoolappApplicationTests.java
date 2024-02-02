44e6acd86e939b379adb9d16eca7a5ac65b1d944
mohamedtamer0 <mt4452242@gmail.com>
2024-02-02 17:14:42 +0200

    SpringBoot

---
+++ 01-spring-boot-overview/05-command-line-demo/src/test/java/com/tamer/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamer.springboot.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
