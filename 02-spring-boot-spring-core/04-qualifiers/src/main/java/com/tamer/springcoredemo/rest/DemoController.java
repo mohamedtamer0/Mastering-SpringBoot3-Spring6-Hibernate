c377758728a93e8c30f1a92fd922895e4c0ead19
mohamedtamer0 <mt4452242@gmail.com>
2024-02-03 18:08:51 +0200

    SpringBoot

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/tamer/springcoredemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.tamer.springcoredemo.rest;
+
+import com.tamer.springcoredemo.Common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    @Autowired
+    public void setCoach(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
---
+++ 02-spring-boot-spring-core/04-qualifiers/src/test/java/com/tamer/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamer.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
