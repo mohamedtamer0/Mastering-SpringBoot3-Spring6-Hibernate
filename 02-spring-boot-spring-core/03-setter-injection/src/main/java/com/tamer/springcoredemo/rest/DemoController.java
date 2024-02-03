e39199f7b1bb82f23cd851253d6ce443eed0eaf6
mohamedtamer0 <mt4452242@gmail.com>
2024-02-03 16:18:11 +0200

    SpringBoot

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/tamer/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.tamer.springcoredemo.rest;
+
+import com.tamer.springcoredemo.Common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define a Constructor for dependency injection
+    @Autowired
+    public DemoController(Coach myCoach) {
+        this.myCoach = myCoach;
+    }
+
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
