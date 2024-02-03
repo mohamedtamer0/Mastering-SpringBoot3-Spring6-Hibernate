b24c37f6a0dc6291b73da51592d1c07ed82d03f8
mohamedtamer0 <mt4452242@gmail.com>
2024-02-03 13:48:12 +0200

    SpringBoot

---
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/tamer/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,13 @@
+package com.tamer.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
