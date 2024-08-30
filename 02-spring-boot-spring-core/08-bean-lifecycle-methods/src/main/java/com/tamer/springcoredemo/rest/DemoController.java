079e0608941b30955aa830ffd528beb75117d685
mohamedtamer0 <mt4452242@gmail.com>
2024-08-30 21:15:55 +0300

    update

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/tamer/springcoredemo/rest/DemoController.java
@@ -0,0 +1,27 @@
+package com.tamer.springcoredemo.rest;
+
+import com.tamer.springcoredemo.Common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(
+                @Qualifier("cricketCoach") Coach theCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
