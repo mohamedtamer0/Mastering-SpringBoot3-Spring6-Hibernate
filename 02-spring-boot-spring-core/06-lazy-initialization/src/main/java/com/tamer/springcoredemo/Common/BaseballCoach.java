fe1230d94c33bfa089eb1a876f1c26e45924c7e0
mohamedtamer0 <mt4452242@gmail.com>
2024-08-30 11:52:57 +0300

    update

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/tamer/springcoredemo/Common/BaseballCoach.java
@@ -0,0 +1,12 @@
+package com.tamer.springcoredemo.Common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes in batting practice";
+    }
+}
