bbdc623a0aece50565758fefd62f951b175bec63
mohamedtamer0 <mt4452242@gmail.com>
2024-09-01 00:28:33 +0300

    update

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/tamer/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/tamer/cruddemo/CruddemoApplication.java
@@ -1,13 +1,22 @@
 package com.tamer.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
 }
