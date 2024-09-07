fb46a80fc145860a2680c0cdebaef32130cafbe8
mohamedtamer0 <mt4452242@gmail.com>
2024-09-07 11:06:26 +0300

    update

---
+++ 03-spring-boot-hibernate-jpa-crud/03-cruddemo-student-query-findAll/src/main/java/com/tamer/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,15 @@
+package com.tamer.cruddemo.dao;
+
+import com.tamer.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+}
