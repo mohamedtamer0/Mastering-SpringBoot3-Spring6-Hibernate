941c7cbf380aafbf2480fd881f61b3bbc8142559
mohamedtamer0 <mt4452242@gmail.com>
2024-09-07 11:07:58 +0300

    update

---
+++ 03-spring-boot-hibernate-jpa-crud/04-cruddemo-student-query-findByLastName/src/main/java/com/tamer/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,17 @@
+package com.tamer.cruddemo.dao;
+
+import com.tamer.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+}
