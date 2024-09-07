e6579f52e2b4dae50556b97d8a7c7b1881d4d1e0
mohamedtamer0 <mt4452242@gmail.com>
2024-09-07 13:28:07 +0300

    update

---
+++ 03-spring-boot-hibernate-jpa-crud/08-cruddemo-create-db-tables-automatically/src/main/java/com/tamer/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,23 @@
+package com.tamer.cruddemo.dao;
+
+import com.tamer.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    int deleteAll();
+
+}
