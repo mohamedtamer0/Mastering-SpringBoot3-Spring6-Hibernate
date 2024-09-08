0c7b9c9918489afa7a16fbf56d909c5ba43b02c2
mohamedtamer0 <mt4452242@gmail.com>
2024-09-08 23:14:15 +0300

    update

--- 04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-dao-find-add-update-delete/src/main/java/com/tamer/springboot/cruddemo/dao/EmployeeDAO.java
+++ 04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-dao-find-add-update-delete/src/main/java/com/tamer/springboot/cruddemo/dao/EmployeeDAO.java
@@ -8,4 +8,9 @@ public interface EmployeeDAO {
 
     List<Employee> findAll();
 
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
 }
--- 04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-dao-find-add-update-delete/src/main/java/com/tamer/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ 04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-dao-find-add-update-delete/src/main/java/com/tamer/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -34,4 +34,34 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
         // return the results
         return employees;
     }
+
+    @Override
+    public Employee findById(int theId) {
+
+        // get employee
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+
+        // return employee
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+
+        // save employee
+        Employee dbEmployee = entityManager.merge(theEmployee);
+
+        // return the dbEmployee
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
+        // find employee by id
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+
+        // remove employee
+        entityManager.remove(theEmployee);
+    }
 }
