2937a25bbb786ada49716472fbaffcd97d7a2775
mohamedtamer0 <mt4452242@gmail.com>
2024-09-08 23:12:50 +0300

    update

---
+++ 04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-dao-find-add-update-delete/src/main/java/com/tamer/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,24 @@
+package com.tamer.springboot.cruddemo.service;
+
+import com.tamer.springboot.cruddemo.dao.EmployeeDAO;
+import com.tamer.springboot.cruddemo.entitiy.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
---
+++ 04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-dao-find-add-update-delete/src/test/java/com/tamer/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamer.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
