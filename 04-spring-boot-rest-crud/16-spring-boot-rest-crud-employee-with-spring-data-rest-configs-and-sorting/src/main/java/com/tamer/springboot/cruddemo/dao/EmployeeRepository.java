14872ef648c2cd66990963eefadf7a136c34ea2c
mohamedtamer0 <mt4452242@gmail.com>
2024-09-09 10:45:53 +0300

    Add new employee REST controllers and configurations with sorting

---
+++ 04-spring-boot-rest-crud/16-spring-boot-rest-crud-employee-with-spring-data-rest-configs-and-sorting/src/main/java/com/tamer/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.tamer.springboot.cruddemo.dao;
+
+import com.tamer.springboot.cruddemo.entitiy.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// @RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}
