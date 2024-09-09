4fee8535c6ae046b7debec8c182cf9e078f3dcff
mohamedtamer0 <mt4452242@gmail.com>
2024-09-09 10:49:02 +0300

    Add employee REST controller with update and delete functionality

---
+++ 04-spring-boot-rest-crud/13-rest-controller-update-employee-and-delete-employee/src/main/java/com/tamer/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,43 @@
+package com.tamer.springboot.cruddemo.service;
+
+
+import com.tamer.springboot.cruddemo.dao.EmployeeDAO;
+import com.tamer.springboot.cruddemo.entitiy.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
+    }
+}
