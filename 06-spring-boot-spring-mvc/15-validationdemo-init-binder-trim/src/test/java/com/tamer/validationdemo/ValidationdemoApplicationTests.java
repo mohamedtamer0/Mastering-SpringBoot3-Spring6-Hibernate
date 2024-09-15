6e149dca02c56b65e3baf56040c96fb4c8b59f0a
mohamedtamer0 <mt4452242@gmail.com>
2024-09-16 00:31:22 +0300

    update

---
+++ 06-spring-boot-spring-mvc/15-validationdemo-init-binder-trim/src/test/java/com/tamer/validationdemo/ValidationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamer.validationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
