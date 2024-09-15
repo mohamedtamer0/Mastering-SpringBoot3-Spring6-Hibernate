6af11e87ff538f83130e2444a4c608e40aab0c91
mohamedtamer0 <mt4452242@gmail.com>
2024-09-16 01:04:33 +0300

    update

---
+++ 06-spring-boot-spring-mvc/17-validationdemo-regular-expressions/src/main/java/com/tamer/validationdemo/ValidationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.tamer.validationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationdemoApplication.class, args);
+	}
+
+}
