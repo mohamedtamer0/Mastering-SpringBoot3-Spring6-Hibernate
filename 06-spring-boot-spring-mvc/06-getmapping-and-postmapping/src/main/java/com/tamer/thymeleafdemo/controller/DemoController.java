51cfb1310524346d6b6599546eaccc805652d803
mohamedtamer0 <mt4452242@gmail.com>
2024-09-15 18:48:37 +0300

    update

---
+++ 06-spring-boot-spring-mvc/06-getmapping-and-postmapping/src/main/java/com/tamer/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.tamer.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    // create a mapping for "/hello"
+
+    @GetMapping("/hello")
+    public String sayHello(Model theModel) {
+
+        theModel.addAttribute("theDate", java.time.LocalDateTime.now());
+
+        return "helloworld";
+    }
+}
