f64252f03fd963b9f20306c867f567d3c273e87d
mohamedtamer0 <mt4452242@gmail.com>
2024-09-15 20:00:48 +0300

    update

---
+++ 06-spring-boot-spring-mvc/07-form-data-binding-textfields/src/main/java/model/Student.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+
+    public Student() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
