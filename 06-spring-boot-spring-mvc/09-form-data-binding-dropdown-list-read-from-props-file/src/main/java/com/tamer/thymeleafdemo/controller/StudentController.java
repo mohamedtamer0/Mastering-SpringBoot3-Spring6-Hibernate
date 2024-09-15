eddb02891640f64d9c03f7c5ee76033ba2be32ad
mohamedtamer0 <mt4452242@gmail.com>
2024-09-15 20:12:00 +0300

    update

---
+++ 06-spring-boot-spring-mvc/09-form-data-binding-dropdown-list-read-from-props-file/src/main/java/com/tamer/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,43 @@
+package com.tamer.thymeleafdemo.controller;
+
+import com.tamer.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel) {
+
+        // create a student object
+        Student theStudent = new Student();
+
+        // add student object to the model
+        theModel.addAttribute("student", theStudent);
+
+        // add the list of countries to the model
+        theModel.addAttribute("countries", countries);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+
+        // log the input data
+        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+
+        return "student-confirmation";
+    }
+
+}
