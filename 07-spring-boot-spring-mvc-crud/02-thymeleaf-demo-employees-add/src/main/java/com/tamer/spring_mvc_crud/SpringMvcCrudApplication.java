3628783b872a8754ea3945728e11a44dac5ec5f8
mohamedtamer0 <mt4452242@gmail.com>
2024-09-16 17:57:45 +0300

    update

---
+++ 07-spring-boot-spring-mvc-crud/02-thymeleaf-demo-employees-add/src/main/java/com/tamer/spring_mvc_crud/SpringMvcCrudApplication.java
@@ -0,0 +1,13 @@
+package com.tamer.spring_mvc_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcCrudApplication.class, args);
+	}
+
+}
