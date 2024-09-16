34fe0f5d3f1cd2549174fef8d6e63c12c043541f
mohamedtamer0 <mt4452242@gmail.com>
2024-09-16 18:13:08 +0300

    update

---
+++ 07-spring-boot-spring-mvc-crud/03-thymeleaf-demo-employees-update/src/main/java/com/tamer/spring_mvc_crud/service/EmployeeServiceImpl.java
@@ -0,0 +1,54 @@
+package com.tamer.spring_mvc_crud.service;
+
+import com.tamer.spring_mvc_crud.dao.EmployeeRepository;
+import com.tamer.spring_mvc_crud.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+        employeeRepository = theEmployeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAllByOrderByLastNameAsc();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        Optional<Employee> result = employeeRepository.findById(theId);
+
+        Employee theEmployee = null;
+
+        if (result.isPresent()) {
+            theEmployee = result.get();
+        }
+        else {
+            // we didn't find the employee
+            throw new RuntimeException("Did not find employee id - " + theId);
+        }
+
+        return theEmployee;
+    }
+
+    @Override
+    public void save(Employee theEmployee) {
+        employeeRepository.save(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        employeeRepository.deleteById(theId);
+    }
+
+}
