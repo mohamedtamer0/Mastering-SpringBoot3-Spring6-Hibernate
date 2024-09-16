5ee7154c12647d04b8685ac491d7ebca49c6cb2c
mohamedtamer0 <mt4452242@gmail.com>
2024-09-16 18:16:51 +0300

    update

---
+++ 07-spring-boot-spring-mvc-crud/04-thymeleaf-demo-employees-delete/src/main/java/com/tamer/spring_mvc_crud/dao/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.tamer.spring_mvc_crud.dao;
+
+import com.tamer.spring_mvc_crud.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+    // add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
+
+}
