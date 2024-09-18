f9c684d52998c5196efcb91fa93965650e38ebe2
mohamedtamer0 <mt4452242@gmail.com>
2024-09-18 18:01:46 +0300

    update

---
+++ 08-spring-boot-spring-mvc-security/08-spring-boot-spring-mvc-security-user-roles-restrict-access/src/test/java/com/tamer/demosecurity/DemosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamer.demosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
