e6e8368c60ae12efad5f2aa585accb73c21d1996
mohamedtamer0 <mt4452242@gmail.com>
2024-09-18 20:23:27 +0300

    update

--- 08-spring-boot-spring-mvc-security/13-spring-boot-spring-mvc-security-jdbc-bcrypt-custom-tables/src/main/java/com/tamer/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/13-spring-boot-spring-mvc-security-jdbc-bcrypt-custom-tables/src/main/java/com/tamer/demosecurity/security/DemoSecurityConfig.java
@@ -17,7 +17,17 @@ public class DemoSecurityConfig {
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?");
+
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
