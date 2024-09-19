7f531b1d3550d52305a3fb7504477b0b4b303fec
mohamedtamer0 <mt4452242@gmail.com>
2024-09-19 20:31:52 +0300

    update

---
+++ 09-spring-boot-jpa-advanced-mappings/07-jpa-one-to-many-create-instructor-with-courses/src/test/java/com/tamer/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamer.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/08-jpa-one-to-many-eager-find-instructor-with-courses/src/main/java/com/tamer/cruddemo/dao/AppDAO.java
@@ -0,0 +1,18 @@
+package com.tamer.cruddemo.dao;
+
+import com.tamer.cruddemo.entity.Instructor;
+import com.tamer.cruddemo.entity.InstructorDetail;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+}
