dd48c86c975ca86721d927d8245ee8ef390369f3
mohamedtamer0 <mt4452242@gmail.com>
2024-09-19 20:46:28 +0300

    update

---
+++ 09-spring-boot-jpa-advanced-mappings/09-jpa-one-to-many-lazy-find-courses-for-instructor/src/main/java/com/tamer/cruddemo/dao/AppDAO.java
@@ -0,0 +1,24 @@
+package com.tamer.cruddemo.dao;
+
+import com.tamer.cruddemo.entity.Course;
+import com.tamer.cruddemo.entity.Instructor;
+import com.tamer.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+
+}
