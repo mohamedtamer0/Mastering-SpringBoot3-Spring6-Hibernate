31f3b73c59674eb98d749ba2d130a1bfebefbc72
mohamedtamer0 <mt4452242@gmail.com>
2024-09-19 23:58:03 +0300

    update

---
+++ 09-spring-boot-jpa-advanced-mappings/20-jpa-many-to-many-find-student-and-courses/src/main/java/com/tamer/cruddemo/dao/AppDAO.java
@@ -0,0 +1,41 @@
+package com.tamer.cruddemo.dao;
+
+import com.tamer.cruddemo.entity.Course;
+import com.tamer.cruddemo.entity.Instructor;
+import com.tamer.cruddemo.entity.InstructorDetail;
+import com.tamer.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor tempInstructor);
+
+    void update(Course tempCourse);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
+
+    void save(Course theCourse);
+
+    Course findCourseAndReviewsByCourseId(int theId);
+
+    Course findCourseAndStudentsByCourseId(int theId);
+
+    Student findStudentAndCoursesByStudentId(int theId);
+}
