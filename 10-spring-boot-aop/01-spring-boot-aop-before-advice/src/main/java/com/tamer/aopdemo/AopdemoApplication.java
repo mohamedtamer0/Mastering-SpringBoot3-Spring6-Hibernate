27c86e913365b9dce819459f97e642885f09c78d
mohamedtamer0 <mt4452242@gmail.com>
2024-09-20 18:36:08 +0300

    update

--- 10-spring-boot-aop/01-spring-boot-aop-before-advice/src/main/java/com/tamer/aopdemo/AopdemoApplication.java
+++ 10-spring-boot-aop/01-spring-boot-aop-before-advice/src/main/java/com/tamer/aopdemo/AopdemoApplication.java
@@ -1,6 +1,7 @@
 package com.tamer.aopdemo;
 
 import com.tamer.aopdemo.dao.AccountDAO;
+import com.tamer.aopdemo.dao.MembershipDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,29 +14,23 @@ public class AopdemoApplication {
 		SpringApplication.run(AopdemoApplication.class, args);
 	}
 
-
-
 	@Bean
-	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
 
 		return runner -> {
 
-			demoTheBeforeAdvice(theAccountDAO);
+			demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
 		};
 	}
 
-	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
 
 		// call the business method
 		theAccountDAO.addAccount();
 
-		// do it again!
-		System.out.println("\n let's call it again!\n");
-
-		// call the business method again
-		theAccountDAO.addAccount();
+		// call the membership business method
+		theMembershipDAO.addAccount();
 
 	}
-
 
 }
---
+++ 10-spring-boot-aop/01-spring-boot-aop-before-advice/src/main/java/com/tamer/aopdemo/dao/MembershipDAOImpl.java
@@ -0,0 +1,14 @@
+package com.tamer.aopdemo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MembershipDAOImpl implements MembershipDAO {
+
+    @Override
+    public void addAccount() {
+
+        System.out.println(getClass() + ": DOING MY DB WORK: ADDING A MEMBERSHIP ACCOUNT");
+
+    }
+}
