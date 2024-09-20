db62c8478d9e3d20a02cf97adb3ffea4157b0abf
mohamedtamer0 <mt4452242@gmail.com>
2024-09-20 18:41:56 +0300

    update

---
+++ 10-spring-boot-aop/03-spring-boot-aop-pointcut-expressions-match-only-accountdao-add-account/src/main/java/com/tamer/aopdemo/dao/AccountDAOImpl.java
@@ -0,0 +1,14 @@
+package com.tamer.aopdemo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AccountDAOImpl implements AccountDAO {
+
+    @Override
+    public void addAccount() {
+
+        System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
+
+    }
+}
