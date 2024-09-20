31edf14cf0d9f1ee35db3aa310514a6ad8a8b89e
mohamedtamer0 <mt4452242@gmail.com>
2024-09-20 16:35:20 +0300

    update

--- 10-spring-boot-aop/01-spring-boot-aop-before-advice/src/main/java/com/tamer/aopdemo/AopdemoApplication.java
+++ 10-spring-boot-aop/01-spring-boot-aop-before-advice/src/main/java/com/tamer/aopdemo/AopdemoApplication.java
@@ -1,7 +1,10 @@
 package com.tamer.aopdemo;
 
+import com.tamer.aopdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopdemoApplication {
@@ -9,5 +12,30 @@ public class AopdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AopdemoApplication.class, args);
 	}
+
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
+
+		return runner -> {
+
+			demoTheBeforeAdvice(theAccountDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+
+		// call the business method
+		theAccountDAO.addAccount();
+
+		// do it again!
+		System.out.println("\n let's call it again!\n");
+
+		// call the business method again
+		theAccountDAO.addAccount();
+
+	}
+
 
 }
