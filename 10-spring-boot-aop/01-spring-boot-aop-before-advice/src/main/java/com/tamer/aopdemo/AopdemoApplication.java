ee3c9c844765be5945c4c8b4f9c68e930e767d36
mohamedtamer0 <mt4452242@gmail.com>
2024-09-20 15:55:23 +0300

    update

---
+++ 10-spring-boot-aop/01-spring-boot-aop-before-advice/src/main/java/com/tamer/aopdemo/AopdemoApplication.java
@@ -0,0 +1,13 @@
+package com.tamer.aopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+}
