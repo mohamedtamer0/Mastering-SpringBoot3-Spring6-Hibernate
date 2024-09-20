9473437140dd22e5735723409c46659ca4f7a43c
mohamedtamer0 <mt4452242@gmail.com>
2024-09-20 22:18:33 +0300

    update

---
+++ 10-spring-boot-aop/10-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/tamer/aopdemo/AopdemoApplication.java
@@ -0,0 +1,39 @@
+package com.tamer.aopdemo;
+
+import com.tamer.aopdemo.dao.AccountDAO;
+import com.tamer.aopdemo.dao.MembershipDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+
+		return runner -> {
+
+			demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+
+		// call the business method
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+
+		// call the membership business method
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+
+	}
+
+}
---
+++ 10-spring-boot-aop/10-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/tamer/aopdemo/dao/AccountDAOImpl.java
@@ -0,0 +1,22 @@
+package com.tamer.aopdemo.dao;
+
+import com.tamer.aopdemo.Account;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AccountDAOImpl implements AccountDAO {
+
+    @Override
+    public void addAccount(Account theAccount, boolean vipFlag) {
+
+        System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
+
+    }
+
+    @Override
+    public boolean doWork() {
+
+        System.out.println(getClass() + ": doWork()");
+        return false;
+    }
+}
