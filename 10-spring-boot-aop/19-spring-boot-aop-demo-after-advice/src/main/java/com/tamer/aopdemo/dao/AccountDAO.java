3843bd51870780ea9b7a471da469d73fc07b3be5
mohamedtamer0 <mt4452242@gmail.com>
2024-09-21 19:50:12 +0300

    update

---
+++ 10-spring-boot-aop/19-spring-boot-aop-demo-after-advice/src/main/java/com/tamer/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,27 @@
+package com.tamer.aopdemo.dao;
+
+import com.tamer.aopdemo.Account;
+
+import java.util.List;
+
+public interface AccountDAO {
+
+    // add a new method: findAccounts()
+
+    List<Account> findAccounts();
+
+    List<Account> findAccounts(boolean tripWire);
+
+    void addAccount(Account theAccount, boolean vipFlag);
+
+    boolean doWork();
+
+    public String getName();
+
+    public void setName(String name);
+
+    public String getServiceCode();
+
+    public void setServiceCode(String serviceCode);
+
+}
