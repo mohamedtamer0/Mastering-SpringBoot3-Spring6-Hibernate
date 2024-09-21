8e9fb907d68ac6a9cec561786faeb702bb899aa0
mohamedtamer0 <mt4452242@gmail.com>
2024-09-21 16:43:39 +0300

    update

---
+++ 10-spring-boot-aop/14-spring-boot-aop-demo-order-aspects/src/main/java/com/tamer/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,18 @@
+package com.tamer.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+    @Before("com.tamer.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+    public void beforeAddAccountAdvice() {
+        System.out.println("\n=====>>> Executing @Before advice on method");
+    }
+
+}
